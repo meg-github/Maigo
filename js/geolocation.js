6836fd441062d23269e69538745e2c31c1efb0ed
meg-github <m_hellob-612@i.softbank.jp>
2014-09-20 13:25:07 +0900

    new
    ファイルあげました

---
+++ js/geolocation.js
@@ -0,0 +1,70 @@
+function GeoLocation() {
+
+}
+
+GeoLocation.prototype.watchCurrentPosition = function(success) {
+    if (navigator.geolocation) {
+        this.watchID = navigator.geolocation.watchPosition(success, null, {
+            enableHighAccuracy: true
+        });
+    } else {
+        window.alert("本ブラウザではGeolocationが使えません");
+    }
+};
+
+GeoLocation.prototype.clearWatchPosition = function() {
+    navigator.geolocation.clearWatch(this.watchID);
+};
+
+GeoLocation.prototype.getGeoDistance = function(lat1, lng1, lat2, lng2, precision) {
+    $('#kyori').empty();
+    var distance = 0;
+    if ((Math.abs(lat1 - lat2) < 0.00001) && (Math.abs(lng1 - lng2) < 0.00001)) {
+        distance = 0;
+    } else {
+        lat1 = lat1 * Math.PI / 180;
+        lng1 = lng1 * Math.PI / 180;
+        lat2 = lat2 * Math.PI / 180;
+        lng2 = lng2 * Math.PI / 180;
+
+        var A = 6378140;
+        var B = 6356755;
+        var F = (A - B) / A;
+
+        var P1 = Math.atan((B / A) * Math.tan(lat1));
+        var P2 = Math.atan((B / A) * Math.tan(lat2));
+
+        var X = Math.acos(Math.sin(P1) * Math.sin(P2) + Math.cos(P1) * Math.cos(P2) * Math.cos(lng1 - lng2));
+        var L = (F / 8) * ((Math.sin(X) - X) * Math.pow((Math.sin(P1) + Math.sin(P2)), 2) / Math.pow(Math.cos(X / 2), 2) - (Math.sin(X) - X) * Math.pow(Math.sin(P1) - Math.sin(P2), 2) / Math.pow(Math.sin(X), 2));
+
+        distance = A * (X + L);
+        var decimal_no = Math.pow(10, precision);
+        distance = Math.round(decimal_no * distance / 1000) / decimal_no; // kmに変換するときは(1000で割る)
+    }
+    return distance;
+};
+
+GeoLocation.prototype.getGeoDirection = function(lat1, lng1, lat2, lng2) {
+    $('#hougaku').empty();
+    // 緯度経度 lat1, lng1 の点を出発として、緯度経度 lat2, lng2 への方位
+    // 北を０度で右回りの角度０～３６０度
+    var Y = Math.cos(lng2 * Math.PI / 180) * Math.sin(lat2 * Math.PI / 180 - lat1 * Math.PI / 180);
+    var X = Math.cos(lng1 * Math.PI / 180) * Math.sin(lng2 * Math.PI / 180) - Math.sin(lng1 * Math.PI / 180) * Math.cos(lng2 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180 - lat1 * Math.PI / 180);
+    var dirE0 = 180 * Math.atan2(Y, X) / Math.PI; // 東向きが０度の方向
+    if (dirE0 < 0) {
+        dirE0 = dirE0 + 360; //0～360 にする。
+    }
+    var dirN0 = (dirE0 + 90) % 360; //(dirE0+90)÷360の余りを出力 北向きが０度の方向
+
+    var directNum = dirN0 / 45;
+    var direct = "";
+    var directArray = ["北", "北東", "東", "南東", "南", "南西", "西", "北西"];
+    for (var i = 0; i < directArray.length; i++) {
+        var from = (7.5 + i) % 8;
+        var to = (0.5 + i) % 8;
+        if (directNum > from && directNum < to) {
+            direct = directArray[i];
+        }
+    }
+    return direct;
+};
---
+++ js/main.js
@@ -0,0 +1,109 @@
+var App = {};
+var PRECISION = 1;
+
+$(document).on('pageinit', '#top', function() {
+        $.ajax({
+           url: 'csvData/data-new1.csv',  //ファイルの場所を指定
+           type: 'get',
+            dataType:'text',  //読み込む形式を指定
+            // header: false, //ヘッダーの扱い
+           async: false
+       })
+
+       .success(function(data){
+          //csvを配列に入れる
+          var csv = $.csv.toArrays(data);
+          //csvを改行コード区切りのデータにする
+          var lines = data.split(/\r\n|\r|\n/);
+          //1からデータ数までの乱数を発生させる
+          var random = (Math.floor(Math.random()*lines.length)+1);
+          //選ばれたデータをfactorに入れる
+          var factor = lines[random];
+          //factorに入っているデータをカンマで区切る
+          var view = factor.split(",");
+
+//施設名ひらがなをぐちゃぐちゃに表示する
+          //施設名ひらがなをsourceに入れる
+          var source = view[3];
+          //一文字ごとに区切る
+          var gucha = source.split('');
+          //適当な順番にソートする
+          gucha.sort(
+            function(){
+              return Math.random()*100 - Math.random()*100;
+            });
+          //ソートされた文字を違う変数に仮置き
+          var karioki = gucha
+          //一文字区切りのデータを結合する
+          App.randomString = karioki.join("・"); 
+
+
+    App.goalLong = view[0]; // 経度
+    App.goalLat = view[1]; // 緯度
+    App.geoLocation = new GeoLocation();
+
+    console.log('Loaded Top Page');
+    console.log(lines[random]); //発生した乱数の行を読み込む
+
+})
+
+});
+
+$(document).on('pageinit', '#main', function() {
+    $('#random').append(App.randomString);
+    console.log(App.randomString);
+
+    App.geoLocation.watchCurrentPosition(function(pos) {
+        var currentLat = pos.coords.latitude;
+        var currentLong = pos.coords.longitude;
+
+        App.distance = App.geoLocation.getGeoDistance(App.goalLat, App.goalLong, currentLat, currentLong, PRECISION);
+        $('#kyori').append(App.distance);
+        console.log(App.distance + "km"); // 距離(km)
+
+        App.direction = App.geoLocation.getGeoDirection(App.goalLat, App.goalLong, currentLat, currentLong);
+        $('#hougaku').append(App.direction);
+        console.log(App.direction); // 方位(e.g., 北, 南)
+
+ 
+
+        // if(App.direction="北"){
+        //   $('#abc').append("ほうがくううううううう");
+        //   App.gazou="imgs/up_center.png";
+        // }else if(App.direction="南"){
+        //   $('#abc').append("ほうがく");
+        //   App.gazou="imgs/low_center.png";
+        // }else if(App.direction="西"){
+        //   $('#abc').append("ほうがくう2344う");
+        //   App.gazou="imgs/center_left.png";
+        // }else if(App.direction="東"){
+        //   $('#abc').append("ほうがくうう7ye4iurtcins");
+        //   App.gazou="imgs/center_right.png";
+        // }else if(App.direction="北東"){
+        //   $('#abc').append("ほうがwwaaefawef");
+        // }else if(App.direction="北西"){
+        //   $('#abc').append("ほうがくljlkjl");
+        // }else if(App.direction="南東"){
+        //   App.gazou="imgs/up_center.png";        
+        // }else(App.direction="南西"){
+        //   App.gazou="imgs/up_center.png";
+
+        // }
+
+
+
+
+    });
+    console.log('Loaded Main Page');
+});
+
+$(document).on('pageshow', '#main', function() {
+
+    console.log('Loaded Main Page');
+});
+
+$(document).on('pageinit', '#goal', function() {
+
+    console.log('Loaded Goal Page');
+});
+
